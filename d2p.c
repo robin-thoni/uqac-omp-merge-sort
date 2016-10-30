bdc6c2319d57749384343cc5f31d7f0ac64d9b8f
Robin THONI <robin@rthoni.com>
2016-10-30 11:56:07 -0400

    switched to tasks

--- d2p.c
+++ d2p.c
@@ -1,30 +1,30 @@
+#include <stdio.h>
+#include <omp.h>
 #include "d2p.h"
 #include "common.h"
 #include "d2s.h"
 
-void merge_sort_sub_par(int* array, unsigned l, unsigned h, unsigned threads)
+void merge_sort_sub_par(int* array, unsigned l, unsigned h)
 {
   if (l < h) {
     unsigned m = (l + h) / 2;
-    if (threads > 1) {
-      unsigned n = threads / 2;
-      #pragma omp parallel sections
-      {
-        #pragma omp section
-        merge_sort_sub_par(array, l, m, n);
-        #pragma omp section
-        merge_sort_sub_par(array, m + 1, h, threads - n);
-      }
-    }
-    else {
-      merge_sort_sub_seq(array, l, m);
-      merge_sort_sub_seq(array, m + 1, h);
-    }
+    #pragma omp task
+      merge_sort_sub_par(array, l, m);
+    #pragma omp task
+      merge_sort_sub_par(array, m + 1, h);
+    #pragma omp taskwait
     merge_sort_merge_seq(array, l, m, h);
   }
 }
 
 void merge_sort_par(int* array, unsigned s, unsigned threads)
 {
-  merge_sort_sub_par(array, 0, s - 1, threads);
+  omp_set_num_threads(threads);
+#pragma omp parallel
+  {
+#pragma omp single
+    {
+      merge_sort_sub_par(array, 0, s - 1);
+    }
+  }
 }
--- d2p.h
+++ d2p.h
@@ -1,7 +1,7 @@
 #ifndef D2P_H
 #define D2P_H
 
-void merge_sort_sub_par(int* array, unsigned l, unsigned h, unsigned threads);
+void merge_sort_sub_par(int* array, unsigned l, unsigned h);
 void merge_sort_par(int* array, unsigned s, unsigned threads);
 
 #endif
